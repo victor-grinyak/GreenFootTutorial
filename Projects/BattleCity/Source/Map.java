6bbe840c79cb66ded7ad72304734c976211dec90
Unknown <deathkill2d@gmail.com>
2016-11-05 01:40:24 +1000

    Исправление прорисовки карты

--- Projects/BattleCity/Source/Map.java
+++ Projects/BattleCity/Source/Map.java
@@ -2,8 +2,8 @@ import greenfoot.*;
 import java.awt.Color;
 /**
  * Write a description of class Map here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Map
@@ -23,41 +23,41 @@ public class Map
                                    {0,0,0,0,0,0,0,0,1,0,0,0,0},
                                    {0,0,0,0,0,0,0,0,0,0,0,0,0}
                                  };
-
+ 
     public Map(World BattleCity)
     {
         GreenfootImage background = BattleCity.getBackground();
         background.setColor(Color.WHITE);
         int size = background.getHeight();
-        
+       
         for (int i = 1; i < 14; i++) {
             background.drawLine(64*i, 0, 64*i, size);//832 - размер поля боя
             background.drawLine(0, 64*i, size, 64*i);
         }
-        
+       
         Wall[][][] wall = new Wall[13][13][4];
-        
+       
         int x = 16;
         int y = 16;
-        
+       
         for (int i = 0; i < 13; i++)
         {
             for (int j = 0; j < 13; j++)
             {
                 for ( int k = 0; k < 4; k++ )
                 {
-                    if ( mapObjects[i][j] == -1) {wall[i][j][k] = new Brick();}
-                    if ( mapObjects[i][j] == 1) {wall[i][j][k] = new Stone();}
+                    if ( mapObjects[i][j] == 1) {wall[i][j][k] = new Brick();}
+                    if ( mapObjects[i][j] == 2) {wall[i][j][k] = new Stone();}
                 }
             }
         }
-        
-        
+       
+       
         for (int i = 1; i < 13; i++)
         {
             for (int j = 1; j < 13; j++)
             {
-                if ( mapObjects[i][j] == 1)
+                if ( mapObjects[i][j] == 1 || mapObjects[i][j] == 2 )
                 {
                     int _x = x + 64*j;
                     int _y = y + 64*i;
@@ -68,7 +68,7 @@ public class Map
                             BattleCity.addObject(wall[i][j][k], _x, _y + k*32);
                         }
                         else
-                        {   
+                        {  
                             if ( k == 2)
                             {
                                 BattleCity.addObject(wall[i][j][k], _x + 32, _y + (k - 2)*32);
@@ -83,12 +83,12 @@ public class Map
             }
         }
     }
-
+ 
     /**
      * An example of a method - replace this comment with your own
-     * 
+     *
      * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * @return     the sum of x and y
      */
-    
+   
 }
