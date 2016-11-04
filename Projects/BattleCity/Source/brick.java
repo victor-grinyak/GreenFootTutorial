2ae087fa069b5e45cf07d10218113f54ff4c1a58
Unknown <hitman7020@mail.ru>
2016-11-04 19:07:53 +1000

    Добавлен классы Map, brick, stone

---
+++ Projects/BattleCity/Source/brick.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class brick here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class brick extends Wall
+{
+    /**
+     * Act - do whatever the brick wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
