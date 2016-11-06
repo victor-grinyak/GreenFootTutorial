12262a8b9fdff7c92346e2d394eda694f6ce2488
Unknown <deathkill2d@gmail.com>
2016-11-06 13:12:54 +1000

    Задержка кадров анимации задается временем
    Переписан класс Animation с использованием таймера

--- Projects/BattleCity/Source/Animation.java
+++ Projects/BattleCity/Source/Animation.java
@@ -11,16 +11,19 @@ public class Animation
 {
     private int _current_frame = 0;
     private int _scale = 1;
-    private int _skip = 0;
-    private int _skipCounter = 0;
+    
     private boolean _pause = false;
     private GreenfootImage[] _Frames = null;
+    
+    private Timer timer = new Timer();
 
-    public Animation(String[] frames_imgs, int scale, int skipTicks)
+    public Animation(String[] frames_imgs, int scale, long frameTime)
     {
         _scale = scale;
-        if(skipTicks > 0) _skip = skipTicks;
         setAnimationArray(frames_imgs);
+        
+        timer.setRange(frameTime);
+        timer.reset();
     }
     
     public void setAnimationArray(String[] frames_imgs)
@@ -42,8 +45,19 @@ public class Animation
         }
     }
     
+    public void setFrameTime(long frameTime)
+    {
+        timer.setRange(frameTime);
+    }
+    
+    public long getFrameTime()
+    {
+        return timer.getRange();
+    }
+    
     public void setPause(boolean pause)
     {
+        if(_pause && !pause) timer.reset();
         _pause = pause;
     }
     
@@ -62,11 +76,10 @@ public class Animation
     {
         if(_pause || _Frames == null) return false;
         
-        if(_skipCounter < _skip){
-            ++_skipCounter;
+        if(timer.inRange()){
             return false;
         }else{
-            _skipCounter = 0;
+            timer.reset();
         }
         
         if(++_current_frame >= _Frames.length){
